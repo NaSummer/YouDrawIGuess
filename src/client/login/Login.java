4dc6f4a5247909c8c9310642acdf9400b7e320f2
NaSummer <na_summer@icloud.com>
2015-06-11 16:31:56 +0800

    Finish Login and Register First Time

---
+++ src/client/login/Login.java
@@ -0,0 +1,195 @@
+package client.login;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import client.lobby.Lobby;
+import client.register.Register;
+import client.transmission.Client;
+
+public class Login extends JFrame{
+	
+	private JLabel JLServerAddress;
+	private JLabel JLUserID;
+	private JLabel JLUserPWD;
+	private JTextField inputServerAddress; 
+	private JTextField inputUserID;
+	private JPasswordField inputUserPWD;
+	private JButton btnLogin;
+	private JButton btnRegister;
+	private String[] args;
+	
+	private Login(String[] args) {
+		JLServerAddress = new JLabel("Server");
+		JLUserID = new JLabel("Username");
+		JLUserPWD = new JLabel("Password");
+		inputServerAddress = new JTextField();
+		inputUserID = new JTextField();
+		inputUserPWD = new JPasswordField();
+		btnLogin = new JButton("Login");
+		btnRegister = new JButton("Register");
+		this.args = args;
+		
+		/* set register window */
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension screenSize = tk.getScreenSize();
+		final int WIDTH = 265;
+		final int HIGHT = 210;
+		this.setBounds(((int)screenSize.getWidth()-WIDTH)/2, ((int)screenSize.getHeight()-HIGHT)/2, WIDTH, HIGHT);
+		this.setResizable(false);
+		this.setLayout(null);
+		this.setTitle("Mini IM");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		final int COLUMN1_X = 30;
+		final int BEGINNING_ROW_Y = 30;
+		final int ROW_HEIGHT = 30;
+		final int LABEL_HEIGHT = 20;
+		final int LABEL_WIDTH_1 = 80;
+		final int COLUMN2_X = COLUMN1_X + LABEL_WIDTH_1;
+		final int LABEL_WIDTH_2 = WIDTH - (COLUMN1_X*2+LABEL_WIDTH_1+5);
+		
+		/* set JLable */
+		JLServerAddress.setBounds(COLUMN1_X, BEGINNING_ROW_Y+ROW_HEIGHT*0, LABEL_WIDTH_1, LABEL_HEIGHT);
+		JLUserID.setBounds(COLUMN1_X, BEGINNING_ROW_Y+ROW_HEIGHT*1, LABEL_WIDTH_1, LABEL_HEIGHT);
+		JLUserPWD.setBounds(COLUMN1_X, BEGINNING_ROW_Y+ROW_HEIGHT*2, LABEL_WIDTH_1, LABEL_HEIGHT);
+		
+		/* set JTextField */
+		inputServerAddress.setBounds(COLUMN2_X, BEGINNING_ROW_Y+ROW_HEIGHT*0, LABEL_WIDTH_2, LABEL_HEIGHT);
+		inputUserID.setBounds(COLUMN2_X, BEGINNING_ROW_Y+ROW_HEIGHT*1, LABEL_WIDTH_2, LABEL_HEIGHT);
+		inputUserPWD.setBounds(COLUMN2_X, BEGINNING_ROW_Y+ROW_HEIGHT*2, LABEL_WIDTH_2, LABEL_HEIGHT);
+		inputUserPWD.setEchoChar('*');
+		
+		/* set JButton */ 
+		final int BUTTON_WIGHT = 90;
+		final int BUTTON_HEIGHT = 20;
+//		final int BUTTON_Y = (BEGINNING_ROW_Y+ROW_HEIGHT*3) + (( HIGHT - (BEGINNING_ROW_Y+ROW_HEIGHT*3) )/2) - (BUTTON_HEIGHT/2);
+		final int BUTTON_Y = 135;
+		final int BUTTON_GAP = 20;
+		final int BUTTON_COLUMN1_X = WIDTH/2 - BUTTON_GAP/2 - BUTTON_WIGHT - 3;
+		final int BUTTON_COLUMN2_X = BUTTON_COLUMN1_X + BUTTON_WIGHT + BUTTON_GAP;
+		btnLogin.setBounds(BUTTON_COLUMN1_X, BUTTON_Y, BUTTON_WIGHT, BUTTON_HEIGHT);
+		btnRegister.setBounds(BUTTON_COLUMN2_X, BUTTON_Y, BUTTON_WIGHT, BUTTON_HEIGHT);
+		
+		/* add inputUserPWD Enter KeyListener */
+		inputUserPWD.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+					/* get input from JTextField */
+					String serverAddress = inputServerAddress.getText();
+					String username = inputUserID.getText();
+					String password = new String(inputUserPWD.getPassword());
+					
+					tryLogin(serverAddress, username, password);
+				}
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		/* add Login button Action Listener */
+		btnLogin.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+//				/* coder */
+//				SHA encode = new SHA();
+				
+				/* get input from JTextField */
+				String serverAddress = inputServerAddress.getText();
+				String username = inputUserID.getText();
+				String password = new String(inputUserPWD.getPassword());
+//				String password = "";
+//				try {
+//					password = new String(encode.encryptSHA(inputUserPWD.getPassword().toString().getBytes()));
+//				} catch (Exception e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+				
+				tryLogin(serverAddress, username, password);
+			}
+		});
+		
+		/* add Register button Action Listener*/
+		btnRegister.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Login.this.dispose();
+				new Register(Login.this, args);
+				//TODO
+			}
+		});
+		
+		this.add(JLServerAddress);
+		this.add(JLUserID);
+		this.add(JLUserPWD);
+		this.add(inputServerAddress);
+		this.add(inputUserID);
+		this.add(inputUserPWD);
+		this.add(btnLogin);
+		this.add(btnRegister);
+		this.setVisible(true);
+	}
+	
+	/*  */
+	private void tryLogin(String serverAddress, String username, String password) {
+		
+		/* judge correctness of input */
+		if (serverAddress.equals("")) {
+			JOptionPane.showMessageDialog(null, JLServerAddress.getText()+" can't be blank.");
+		} else if (username.equals("")) {
+			JOptionPane.showMessageDialog(null, JLUserID.getText()+" can't be blank.");
+		} else if (password.equals("")) {
+			JOptionPane.showMessageDialog(null, JLUserPWD.getText()+" can't be blank.");
+		} else {
+			
+			/* create new client */
+			Client client = new Client(serverAddress);
+			
+			
+			/* get the correctness of username and password from server */
+			if (client.login(username, password)) {
+				Login.this.dispose(); // close login window
+				new Lobby(client).main(args);; // open new Lobby window
+			} else {
+				JOptionPane.showMessageDialog(null, "Cannot connect to server(" + serverAddress + ") or incorrect Username or Password.");
+			}
+		}
+	}
+	
+	/*  */
+	public void afterRegister(String serverAddress, String username) {
+		inputServerAddress.setText(serverAddress);
+		inputUserID.setText(username);
+	}
+	
+	/*  */
+	public static void main(String[] args) {
+		new Login(args);
+	}
+
+}
---
+++ src/client/register/Register.java
@@ -0,0 +1,255 @@
+package client.register;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import client.lobby.Lobby;
+import client.login.Login;
+import client.transmission.Client;
+
+
+public class Register extends JFrame {
+	
+	private JLabel JLServerAddress;
+	private JLabel JLUserID;
+	private JLabel JLUserPWD;
+	private JLabel JLUserPWDConfirm;
+	private JTextField JTFServerAddress;
+	private JTextField JTFUserID;
+	private JPasswordField JPFUserPWD;
+	private JPasswordField JPFUserPWDConfirm;
+	private JButton btnRegister;
+	private JButton btnCancel;
+	private String[] args;
+	
+	Login loginWindow;
+	
+	public Register(Login loginWindow, String[] args) {
+		
+		this.loginWindow = loginWindow;
+		this.args = args;
+		
+		JLServerAddress = new JLabel("Server");
+		JLUserID = new JLabel("Username");
+		JLUserPWD = new JLabel("Password");
+		JLUserPWDConfirm = new JLabel("Password Confirm");
+		JTFServerAddress = new JTextField();
+		JTFUserID = new JTextField();
+		JPFUserPWD = new JPasswordField();
+		JPFUserPWDConfirm = new JPasswordField();
+		btnRegister = new JButton("Register");
+		btnCancel = new JButton("Cancel");
+		
+		// set login window
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension screenSize = tk.getScreenSize();
+		final int WIDTH = 305;
+		final int HIGHT = 240;
+		this.setBounds(((int)screenSize.getWidth()-WIDTH)/2, ((int)screenSize.getHeight()-HIGHT)/2, WIDTH, HIGHT);
+		this.setResizable(false);
+		this.setLayout(null);
+		this.setTitle("Register");
+		this.addWindowListener(new WindowListener() {
+			
+			@Override
+			public void windowOpened(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowIconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowClosing(WindowEvent e) {
+				Register.this.dispose();
+				Register.this.loginWindow.setVisible(true);
+			}
+			
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowActivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		final int COLUMN1_X = 30;
+		final int BEGINNING_ROW_Y = 30;
+		final int ROW_HEIGHT = 30;
+		final int LABEL_HEIGHT = 20;
+		final int LABEL_WIDTH_1 = 120;
+		final int COLUMN2_X = COLUMN1_X + LABEL_WIDTH_1;
+		final int LABEL_WIDTH_2 = WIDTH - (COLUMN1_X*2+LABEL_WIDTH_1+5);
+		
+		/* set JLable */
+		JLServerAddress.setBounds(COLUMN1_X, BEGINNING_ROW_Y+ROW_HEIGHT*0, LABEL_WIDTH_1, LABEL_HEIGHT);
+		JLUserID.setBounds(COLUMN1_X, BEGINNING_ROW_Y+ROW_HEIGHT*1, LABEL_WIDTH_1, LABEL_HEIGHT);
+		JLUserPWD.setBounds(COLUMN1_X, BEGINNING_ROW_Y+ROW_HEIGHT*2, LABEL_WIDTH_1, LABEL_HEIGHT);
+		JLUserPWDConfirm.setBounds(COLUMN1_X, BEGINNING_ROW_Y+ROW_HEIGHT*3, LABEL_WIDTH_1, LABEL_HEIGHT);
+		
+		/* set JTextField */
+		JTFServerAddress.setBounds(COLUMN2_X, BEGINNING_ROW_Y+ROW_HEIGHT*0, LABEL_WIDTH_2, LABEL_HEIGHT);
+		JTFUserID.setBounds(COLUMN2_X, BEGINNING_ROW_Y+ROW_HEIGHT*1, LABEL_WIDTH_2, LABEL_HEIGHT);
+		JPFUserPWD.setBounds(COLUMN2_X, BEGINNING_ROW_Y+ROW_HEIGHT*2, LABEL_WIDTH_2, LABEL_HEIGHT);
+		JPFUserPWD.setEchoChar('*');
+		JPFUserPWDConfirm.setBounds(COLUMN2_X, BEGINNING_ROW_Y+ROW_HEIGHT*3, LABEL_WIDTH_2, LABEL_HEIGHT);
+		JPFUserPWDConfirm.setEchoChar('*');
+		
+		/* set JButton */
+		final int BUTTON_WIGHT = 90;
+		final int BUTTON_HEIGHT = 20;
+//		final int BUTTON_Y = (BEGINNING_ROW_Y+ROW_HEIGHT*3) + (( HIGHT - (BEGINNING_ROW_Y+ROW_HEIGHT*3) )/2) - (BUTTON_HEIGHT/2);
+		final int BUTTON_Y = 165;
+		final int BUTTON_GAP = 20;
+		final int BUTTON_COLUMN1_X = WIDTH/2 - BUTTON_GAP/2 - BUTTON_WIGHT - 3;
+		final int BUTTON_COLUMN2_X = BUTTON_COLUMN1_X + BUTTON_WIGHT + BUTTON_GAP;
+		btnRegister.setBounds(BUTTON_COLUMN1_X, BUTTON_Y, BUTTON_WIGHT, BUTTON_HEIGHT);
+		btnCancel.setBounds(BUTTON_COLUMN2_X, BUTTON_Y, BUTTON_WIGHT, BUTTON_HEIGHT);
+		
+		/* add Enter KeyListener to JPFPWDConfirm */
+		JPFUserPWDConfirm.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+					String serverAdd = JTFServerAddress.getText();
+					String username = JTFUserID.getText();
+					String password = new String(JPFUserPWD.getPassword());
+					String confirm = new String(JPFUserPWDConfirm.getPassword());
+					tryRegister(serverAdd, username, password, confirm);
+				}
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		/* add Register button Action Listener */
+		btnRegister.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String serverAdd = JTFServerAddress.getText();
+				String username = JTFUserID.getText();
+				String password = new String(JPFUserPWD.getPassword());
+				String confirm = new String(JPFUserPWDConfirm.getPassword());
+				tryRegister(serverAdd, username, password, confirm);
+			}
+		});
+		
+		/* add Cancel button Action Listener */
+		btnCancel.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				Register.this.dispose();
+				loginWindow.setVisible(true);
+			}
+		});
+		
+		this.add(JLServerAddress);
+		this.add(JLUserID);
+		this.add(JLUserPWD);
+		this.add(JLUserPWDConfirm);
+		this.add(JTFServerAddress);
+		this.add(JTFUserID);
+		this.add(JPFUserPWD);
+		this.add(JPFUserPWDConfirm);
+		this.add(btnRegister);
+		this.add(btnCancel);
+		this.setVisible(true);
+		
+	}
+	
+	private void tryRegister(String serverAdd, String username, String password, String confirm) {
+		if (serverAdd.equals("")) {
+			JOptionPane.showMessageDialog(null, "Server Address can't be blank.");
+		} else 	if (username.equals("")) {
+			JOptionPane.showMessageDialog(null, "Username can't be blank.");
+		} else if (password.equals("")) {
+			JOptionPane.showMessageDialog(null, "Password can't be blank.");
+		} else if (confirm.equals("")) {
+			JOptionPane.showMessageDialog(null, "Password Confirm can't be blank.");
+		} else if (!password.equals(confirm)) {
+			JOptionPane.showMessageDialog(null, "Confirm Password is different from the Password.");
+			JPFUserPWD.setText("");
+			JPFUserPWDConfirm.setText("");
+		} else if (password.length()<6) {
+			JOptionPane.showMessageDialog(null, "Password should be longer than 6 charactors.");
+			JPFUserPWD.setText("");
+			JPFUserPWDConfirm.setText("");
+		} else { 
+			
+//			/* coder */
+//			SHA encoder = new SHA();
+//			
+//			try {
+//				password = new String(encoder.encryptSHA(password.getBytes()));
+//			} catch (Exception e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+			
+			/* create Register Client */
+			Client client = new Client(serverAdd);
+			
+			/* check success */
+			if (client.register(username, password)) {
+				
+				JOptionPane.showMessageDialog(null, "Register Successful.");
+				Register.this.dispose(); // close the Register window.
+				new Lobby(client).main(args);
+				
+			} else {
+				
+				JOptionPane.showMessageDialog(null, "Username is existing.\nPlease try another username.");
+				JTFUserID.requestFocus(); // focus on username TODO
+				JTFUserID.selectAll(); // select username
+				
+			}
+			
+		}
+	}
+
+}
